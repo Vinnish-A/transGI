f28fcb9a4104e22ce6a4bd19f157568637223db0
Vinnish <zhihao_xu_a@163.com>
2024-04-21 01:57:17 +0800

    fix pairwise

--- R/pairwise.R
+++ R/pairwise.R
@@ -9,16 +9,16 @@
 #' @importFrom future nbrOfWorkers
 #'
 #' @keywords internal
-pair.wise = function(mat_, net_, nThreads_ = 1) {
+pair.wise = function(mat_, net_) {
 
   message('Ready to process pairwising...')
   myApply_ = ifelse(nbrOfWorkers() > 1, future_lapply, lapply)
   pairwise_ = myApply_(
     X = 1:nrow(net_),
     FUN = function(i__) {
-      r1__ = which(rownames(mat_) == net_[[1]][i])
-      r2__ = which(rownames(mat_) == net_[[2]][i])
-      r__  = as.numeric(mat_[r1, ] > mat_[r2, ])
+      r1__ = which(rownames(mat_) == net_[[1]][i__])
+      r2__ = which(rownames(mat_) == net_[[2]][i__])
+      r__  = as.numeric(mat_[r1__, ] > mat_[r2__, ])
 
       return(list(as.matrix(net_[i__, ]), r__))
     }
@@ -29,6 +29,8 @@ pair.wise = function(mat_, net_, nThreads_ = 1) {
   rankValue_ = do.call(cbind, sapply(pairwise_, '[', 2))
   genePair_ = cbind(genePair_, paste0('V', 1:nrow(genePair_)))
 
+  rownames(rankValue_) = colnames(mat_)
+  
   return(list(genePair_, rankValue_))
 
 }
